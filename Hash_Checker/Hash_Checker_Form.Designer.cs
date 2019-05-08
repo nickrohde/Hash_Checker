c856168b4c69fa8be792e21267eb32d7639168ba
nickrohde <rohden@cwu.edu>
2019-05-08 13:51:20 -0700

    first commit

---
+++ Hash_Checker/Hash_Checker_Form.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Hash_checker
+{
+    partial class Hash_Checker_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.select_file = new System.Windows.Forms.Button();
+            this.file_name_lbl = new System.Windows.Forms.Label();
+            this.calculate_md5 = new System.Windows.Forms.Button();
+            this.hash_lbl = new System.Windows.Forms.Label();
+            this.verify_hash_checkbox = new System.Windows.Forms.CheckBox();
+            this.expected_hash = new System.Windows.Forms.TextBox();
+            this.expected_lbl = new System.Windows.Forms.Label();
+            this.verification_lbl = new System.Windows.Forms.Label();
+            this.hash_textbox = new System.Windows.Forms.TextBox();
+            this.hash_dropdown = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // select_file
+            // 
+            this.select_file.Location = new System.Drawing.Point(44, 83);
+            this.select_file.Name = "select_file";
+            this.select_file.Size = new System.Drawing.Size(124, 29);
+            this.select_file.TabIndex = 0;
+            this.select_file.Text = "Select File ...";
+            this.select_file.UseVisualStyleBackColor = true;
+            this.select_file.Click += new System.EventHandler(this.Select_file_click);
+            // 
+            // file_name_lbl
+            // 
+            this.file_name_lbl.AutoSize = true;
+            this.file_name_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.file_name_lbl.Location = new System.Drawing.Point(189, 83);
+            this.file_name_lbl.Name = "file_name_lbl";
+            this.file_name_lbl.Size = new System.Drawing.Size(118, 24);
+            this.file_name_lbl.TabIndex = 1;
+            this.file_name_lbl.Text = "Not Selected";
+            // 
+            // calculate_md5
+            // 
+            this.calculate_md5.Location = new System.Drawing.Point(74, 291);
+            this.calculate_md5.Name = "calculate_md5";
+            this.calculate_md5.Size = new System.Drawing.Size(124, 29);
+            this.calculate_md5.TabIndex = 2;
+            this.calculate_md5.Text = "Calculate Hash";
+            this.calculate_md5.UseVisualStyleBackColor = true;
+            this.calculate_md5.Click += new System.EventHandler(this.Calculate_hash_Click);
+            // 
+            // hash_lbl
+            // 
+            this.hash_lbl.AutoSize = true;
+            this.hash_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hash_lbl.Location = new System.Drawing.Point(223, 291);
+            this.hash_lbl.Name = "hash_lbl";
+            this.hash_lbl.Size = new System.Drawing.Size(80, 24);
+            this.hash_lbl.TabIndex = 3;
+            this.hash_lbl.Text = "hash_lbl";
+            this.hash_lbl.Visible = false;
+            // 
+            // verify_hash_checkbox
+            // 
+            this.verify_hash_checkbox.AutoSize = true;
+            this.verify_hash_checkbox.Location = new System.Drawing.Point(74, 165);
+            this.verify_hash_checkbox.Name = "verify_hash_checkbox";
+            this.verify_hash_checkbox.Size = new System.Drawing.Size(80, 17);
+            this.verify_hash_checkbox.TabIndex = 4;
+            this.verify_hash_checkbox.Text = "Verify Hash";
+            this.verify_hash_checkbox.UseVisualStyleBackColor = true;
+            this.verify_hash_checkbox.CheckedChanged += new System.EventHandler(this.Verify_checkbox_changed);
+            // 
+            // expected_hash
+            // 
+            this.expected_hash.Location = new System.Drawing.Point(297, 204);
+            this.expected_hash.Name = "expected_hash";
+            this.expected_hash.Size = new System.Drawing.Size(234, 20);
+            this.expected_hash.TabIndex = 5;
+            this.expected_hash.Visible = false;
+            this.expected_hash.TextChanged += new System.EventHandler(this.Calculate_hash_Click);
+            // 
+            // expected_lbl
+            // 
+            this.expected_lbl.AutoSize = true;
+            this.expected_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.expected_lbl.Location = new System.Drawing.Point(70, 200);
+            this.expected_lbl.Name = "expected_lbl";
+            this.expected_lbl.Size = new System.Drawing.Size(221, 24);
+            this.expected_lbl.TabIndex = 6;
+            this.expected_lbl.Text = "Enter the expected hash:";
+            this.expected_lbl.Visible = false;
+            // 
+            // verification_lbl
+            // 
+            this.verification_lbl.AutoSize = true;
+            this.verification_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verification_lbl.Location = new System.Drawing.Point(293, 240);
+            this.verification_lbl.Name = "verification_lbl";
+            this.verification_lbl.Size = new System.Drawing.Size(108, 24);
+            this.verification_lbl.TabIndex = 7;
+            this.verification_lbl.Text = "verify_result";
+            this.verification_lbl.Visible = false;
+            // 
+            // hash_textbox
+            // 
+            this.hash_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hash_textbox.Location = new System.Drawing.Point(227, 291);
+            this.hash_textbox.Name = "hash_textbox";
+            this.hash_textbox.ReadOnly = true;
+            this.hash_textbox.Size = new System.Drawing.Size(454, 29);
+            this.hash_textbox.TabIndex = 8;
+            this.hash_textbox.Visible = false;
+            // 
+            // hash_dropdown
+            // 
+            this.hash_dropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.hash_dropdown.FormattingEnabled = true;
+            this.hash_dropdown.Items.AddRange(new object[] {
+            "MD5",
+            "SHA1",
+            "SHA256",
+            "SHA512"});
+            this.hash_dropdown.Location = new System.Drawing.Point(208, 35);
+            this.hash_dropdown.Name = "hash_dropdown";
+            this.hash_dropdown.Size = new System.Drawing.Size(193, 21);
+            this.hash_dropdown.TabIndex = 9;
+            this.hash_dropdown.SelectedIndexChanged += new System.EventHandler(this.Hash_dropdown_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Algorithm To Use:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hash_dropdown);
+            this.Controls.Add(this.hash_textbox);
+            this.Controls.Add(this.verification_lbl);
+            this.Controls.Add(this.expected_lbl);
+            this.Controls.Add(this.expected_hash);
+            this.Controls.Add(this.verify_hash_checkbox);
+            this.Controls.Add(this.hash_lbl);
+            this.Controls.Add(this.calculate_md5);
+            this.Controls.Add(this.file_name_lbl);
+            this.Controls.Add(this.select_file);
+            this.Name = "Form1";
+            this.Text = "Hash Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button select_file;
+        private System.Windows.Forms.Label file_name_lbl;
+        private System.Windows.Forms.Button calculate_md5;
+        private System.Windows.Forms.Label hash_lbl;
+        private System.Windows.Forms.CheckBox verify_hash_checkbox;
+        private System.Windows.Forms.TextBox expected_hash;
+        private System.Windows.Forms.Label expected_lbl;
+        private System.Windows.Forms.Label verification_lbl;
+        private System.Windows.Forms.TextBox hash_textbox;
+        private System.Windows.Forms.ComboBox hash_dropdown;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Hash_Checker/Hash_Checker_Form.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Hash_checker
+{
+    public partial class Hash_Checker_Form : Form
+    {
+        #region Private Members:
+        // Read-only members:
+        /// <summary>
+        /// Maximum length of the path being displayed next to the file selection button.
+        /// </summary>
+        private readonly int PATH_DISPLAY_MAX_LENGTH;
+
+        // Data members:
+        /// <summary>
+        /// Selector for which file to hash.
+        /// </summary>
+        private OpenFileDialog open_file_dialog;
+
+        /// <summary>
+        /// The algorithm object being used to hash files.
+        /// </summary>
+        private HashAlgorithm algorithm;
+
+        /// <summary>
+        /// Path to currently selected file.
+        /// </summary>
+        private string file_path;
+        #endregion
+
+
+        #region Constructors:
+        /// <summary>
+        /// Constructor that initializes all components to defaults.
+        /// </summary>
+        public Hash_Checker_Form()
+        {
+            InitializeComponent();
+
+            // initialize readonlys
+            PATH_DISPLAY_MAX_LENGTH = 50;
+
+            // initialize members
+            algorithm = null;
+			file_path = null;
+            open_file_dialog = new OpenFileDialog();
+        } // end Constructor 
+        #endregion
+
+
+        #region Event Handlers:
+        /// <summary>
+        /// On-click event handler for select file button. Opens a file-picker dialog
+        /// and then stores that file name in <see cref="file_path"/>. Also displays
+        /// the first <see cref="PATH_DISPLAY_MAX_LENGTH"/> characters in the 
+        /// <see cref="file_name_lbl"/>.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
+        private void Select_file_click(object sender, EventArgs e)
+        {
+            var result = open_file_dialog.ShowDialog();
+		
+            // ensure file dialog box closed correctly
+            if(result == DialogResult.OK)
+            {
+                // build path for display
+                var path = new StringBuilder();
+                path.Append("Path: ");
+
+                // store path internally for calculating hashes later
+                file_path = open_file_dialog.FileName;
+
+                // avoid displaying long paths that run off-screen
+                if(file_path.Length <= PATH_DISPLAY_MAX_LENGTH)
+                {
+                    path.Append(file_path);
+                } // end if
+                else
+                {
+                    path.Append(file_path.Substring(0, PATH_DISPLAY_MAX_LENGTH));
+                    path.Append("...");
+                } // end else
+
+                // display path to user
+                file_name_lbl.Text = path.ToString();
+            } // end if
+        } // end method select_file_click
+
+
+        /// <summary>
+        /// Calculates the currently selected hash (in <see cref="hash_dropdown"/>)
+        /// for the selected file (in <see cref="file_path"/>) and displays the hash
+        /// string in <see cref="hash_textbox"/> if both inputs were valid.
+        /// If <see cref="verify_hash_checkbox"/> is checked, it also verifies the 
+        /// calculated hash for correctness using <see cref="Verify_hash_values(string)"/>.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
+        private void Calculate_hash_Click(object sender, EventArgs e)
+        {
+            if (file_path == null)
+            {
+                string message = "You did not select a file yet!";
+                string caption = "Error: No File Selected";
+                MessageBoxButtons button = MessageBoxButtons.OK;
+                DialogResult res;
+
+                res = MessageBox.Show(message, caption, button);
+            } // end if
+			else
+			{
+				string hash = Hash_file(file_path);
+
+				if(hash != null)
+				{
+					hash_textbox.Text = hash;
+					hash_textbox.Visible = true;
+
+					if (verify_hash_checkbox.Checked)
+					{
+						Verify_hash_values(hash);
+					} // end if verify_hash_checkbox.Checked
+				} // end if hash != null
+                else
+                {
+                    hash_textbox.Text = string.Empty;
+                    hash_textbox.Visible = false;
+                    verification_lbl.Visible = false;
+                } // end else hash == null
+			} // end else	
+        } // end method calculate_hash_Click
+
+
+        /// <summary>
+        /// On-changed event handler for <see cref="verify_hash_checkbox"/> checkbox.
+        /// Toggles the <see cref="expected_hash"/> textbox, <see cref="expected_lbl"/>
+        /// label, and <see cref="verification_lbl"/>. If the checkbox is checked, these
+        /// are displayed, otherwise they are hidden. The value of <see cref="verification_lbl"/>
+        /// reset to <see cref="string.Empty"/> with each call.
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
+        private void Verify_checkbox_changed(object sender, EventArgs e)
+        {
+            expected_lbl.Visible = verify_hash_checkbox.Checked;
+            expected_hash.Visible = verify_hash_checkbox.Checked;
+            verification_lbl.Visible = verify_hash_checkbox.Checked;
+            verification_lbl.Text = string.Empty;
+        } // end method verify_checkbox_changed
+
+
+        /// <summary>
+        /// On-changed event handler for <see cref="hash_dropdown"/> dropdown list.
+        /// Disposes of the old <see cref="algorithm"/> object (if applicable) and
+        /// instantiates a new one based on the user's selection. 
+        /// </summary>
+        /// <param name="sender">Unused.</param>
+        /// <param name="e">Unused.</param>
+        private void Hash_dropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selected = hash_dropdown.SelectedItem as string;
+
+            // properly dispose of old algorithm object if applicable
+            if (algorithm != null)
+            {
+                algorithm.Dispose();
+                algorithm = null;
+            } // end if
+
+            // instantiate appropriate algorithm object
+            switch (selected)
+            {
+                case "MD5":
+                    algorithm = MD5.Create();
+                    break;
+                case "SHA1":
+                    algorithm = SHA1.Create();
+                    break;
+                case "SHA256":
+                    algorithm = SHA256.Create();
+                    break;
+                case "SHA512":
+                    algorithm = SHA512.Create();
+                    break;
+                default:
+                    // algorithm is already null from if statement above
+                    break;
+            } // end switch
+        } // end method hash_dropdown_SelectedIndexChanged
+        #endregion
+
+
+        #region Utility Functions:
+        /// <summary>
+        /// compares the calculated <paramref name="hash"/> against a string entered by 
+        /// the user into <see cref="expected_hash"/>. If the hashes match, a green message
+        /// is displayed in <see cref="verification_lbl"/>, otherwise a red message is 
+        /// displayed. 
+        /// </summary>
+        /// <param name="hash"></param>
+        private void Verify_hash_values(string hash)
+        {
+            if(hash == expected_hash.Text.ToLowerInvariant())
+            {
+                verification_lbl.Text = "The expected hash is correct!";
+                verification_lbl.Visible = true;
+                verification_lbl.BackColor = Color.Green;
+            } // end if
+            else
+            {
+                verification_lbl.Text = "The expected hash is NOT correct!";
+                verification_lbl.Visible = true;
+                verification_lbl.BackColor = Color.Red;
+            } // end else
+        } // end method verify_hash_values
+
+
+        /// <summary>
+        /// Calculates the hash for a given file located at <paramref name="file_path"/>
+        /// and returns the hash as a hexadecimal string with any separators removed.
+        /// </summary>
+        /// <param name="file_path">The path to the file to check, or null.</param>
+        /// <returns>
+        /// A hexadecimal string if <paramref name="file_path"/> was a valid file path
+        /// and a valid <see cref="HashAlgorithm"/> object was available 
+        /// (in <see cref="algorithm"/> on method invocation, otherwise null.
+        /// </returns>
+        /// <remarks>
+        /// The exact hashing strategy to use is determined by the value of
+        /// <see cref="algorithm"/>.
+        /// </remarks>
+        private string Hash_file(string file_path)
+        {
+            string result = null;
+
+            if(algorithm == null)
+            {
+                string message = "You did not select a hashing algorithm!";
+                string caption = "Error: No Algorithm Selected";
+                MessageBoxButtons button = MessageBoxButtons.OK;
+                DialogResult res = MessageBox.Show(message, caption, button);
+            } // end if
+            else
+            {
+                using (var stream = File.OpenRead(file_path))
+                {
+                    // hash the file and convert the hash to lowercase string without dashes for nice display
+                    result = BitConverter.ToString(algorithm.ComputeHash(stream)).Replace("-", "").ToLowerInvariant();
+                } // end using
+            } // end else
+            
+            return result;
+        } // end method hash_file
+        #endregion
+    } // end class Form1
+} // end namespace MD_5_checker
